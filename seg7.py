ca0dda6c0174871f81395d1e321bc016558f1a45
Chetco <justin.m.brown@intel.com>
2015-04-30 11:22:31 -0700

    pep8

--- seg7.py
+++ seg7.py
@@ -30,23 +30,24 @@ import sys
 
 __all__ = ['Seg7']
 
+
 class Seg7:
 
-    chars = {'1' : 0b10000010, '2' : 0b00011111, '3' : 0b01011101,
-             '4' : 0b11010100, '5' : 0b11001101, '6' : 0b11001111,
-             '7' : 0b01011000, '8' : 0b11011111, '9' : 0b11011100,
-             'a' : 0b11011110, 'b' : 0b11000111, 'c' : 0b00000111,
-             'd' : 0b01010111, 'e' : 0b10001111, 'f' : 0b10001110,
-             '0' : 0b11011011, ' ' : 0b00000000, '.' : 0b00100000,
-             '@' : 0b11111111,
-    }
+    chars = {'1': 0b10000010, '2': 0b00011111, '3': 0b01011101,
+             '4': 0b11010100, '5': 0b11001101, '6': 0b11001111,
+             '7': 0b01011000, '8': 0b11011111, '9': 0b11011100,
+             'a': 0b11011110, 'b': 0b11000111, 'c': 0b00000111,
+             'd': 0b01010111, 'e': 0b10001111, 'f': 0b10001110,
+             '0': 0b11011011, ' ': 0b00000000, '.': 0b00100000,
+             '@': 0b11111111,
+             }
 
     def __init__(self,
                  delay=None,
                  chars=None,
                  clock_pin=25,
                  latch_pin=18,
-                 data_pin =20,
+                 data_pin=20,
                  clear_pin=16):
 
         from mraa import Gpio, DIR_OUT_LOW
@@ -55,7 +56,7 @@ class Seg7:
 
         self.clock_pin = Gpio(clock_pin)
         self.latch_pin = Gpio(latch_pin)
-        self.data_pin  = Gpio(data_pin)
+        self.data_pin = Gpio(data_pin)
         self.clear_pin = Gpio(clear_pin)
 
         self.clock_pin.dir(DIR_OUT_LOW)
@@ -68,33 +69,28 @@ class Seg7:
         self.clear()
         self.putc('.')
 
-
     def _tick(self):
         self.clock_pin.write(1)
         self.clock_pin.write(0)
-
 
     def _shift0(self):
         self.data_pin.write(1)
         self._tick()
 
-
     def _shift1(self):
         self.data_pin.write(0)
         self._tick()
-
 
     def _latch(self):
         self.latch_pin.write(0)
         self.latch_pin.write(1)
         self.latch_pin.write(0)
 
-
     def write(self, byte):
-	"""
+        """
         Explicitly write any arbitrary byte to the seven-segment.
         """
-	if byte not in xrange(0, 256):
+        if byte not in xrange(0, 256):
             raise ValueError("write's argument is not a byte (0-255)")
 
         byte_str = format(byte, "#010b")[2:]
@@ -106,21 +102,18 @@ class Seg7:
                 self._shift0()
         self._latch()
 
-
     def clear(self):
         self.clear_pin.write(1)
         self.clear_pin.write(0)
         self.clear_pin.write(1)
         self.write(0)
 
-
     def set_delay(self, delay):
         self.delay = delay
 
-
     def putc(self, char, delay=None):
         from time import sleep
-        
+
         if delay is None:
             delay = 0.0
 
@@ -129,9 +122,8 @@ class Seg7:
             raise ValueError("Seg7 will only display hexadecimal symbols")
 
         self.write(binary)
-        
-        sleep(delay)
 
+        sleep(delay)
 
     def puts(self, string, delay=None):
         """
@@ -143,7 +135,7 @@ class Seg7:
         if delay is None:
             delay = self.delay
 
-	delay = self.delay if delay is None else delay
+        delay = self.delay if delay is None else delay
 
         for char in string:
             self.putc(char, delay)
@@ -180,7 +172,6 @@ class Seg7:
             self.clear()
             sleep(period * (1.0 - duty))
 
-
     def blink(self, char, period=None, duty=None, elapse=None):
         """
         Blink a character on a duty cycle for an elapsed time.
@@ -201,7 +192,7 @@ class Seg7:
             elapse = 1.0
         elif elapse < 0.0:
             raise ValueError("elapse < 0.0")
-        
+
         start = time()
         finish = start
         while(finish - start < elapse):
@@ -211,22 +202,22 @@ class Seg7:
             finish = time()
 
 
-
 def test_putc(seg):
     print("Calamari Seg7 test: putc")
     # test 'putc' on some of the defined symbols
-    symbols = sorted([sym for sym in seg.chars.keys() if sym not in [' ', '.', '@']])
+    IGNORED = [' ', '.', '@']
+    symbols = sorted([sym for sym in seg.chars.keys() if sym not in IGNORED])
     print("symbols = %s" % symbols)
     for key in symbols:
         seg.putc(key, 0.25)
-        #sleep(0.04)
     seg.clear()
 
 
 def test_puts(seg):
     print("Calamari Seg7 test: puts")
     # test 'puts' on a string containing the same symbols as above
-    symbols = sorted([sym for sym in seg.chars.keys() if sym not in [' ', '.', '@']])
+    IGNORED = [' ', '.', '@']
+    symbols = sorted([sym for sym in seg.chars.keys() if sym not in IGNORED])
     string = ''.join(symbols[::-1])
     print("string = %s" % string)
     seg.puts(string, delay=0.04)
@@ -236,9 +227,9 @@ def test_puts(seg):
 def test_blink_period(seg):
     print("Calamari Seg7 test: blink period")
     # test 'blink' by blinking all segments with decreasing periods
-    from math import pow as  fpow
+    from math import pow as fpow
     for pace in range(0, 7, 1):
-        seg.blink('@', period = 1.0/fpow(2, pace), elapse = 1.0)
+        seg.blink('@', period=1.0/fpow(2, pace), elapse=1.0)
 
 
 def test_blinkc_period(seg):
@@ -246,24 +237,24 @@ def test_blinkc_period(seg):
     # test 'blink' by blinking all segments with decreasing periods
     from math import pow as fpow
     for pace in range(7, 0, -1):
-        seg.blinkc('@', period = 1.0/fpow(2, pace), cycles = 2 ** pace)
+        seg.blinkc('@', period=1.0/fpow(2, pace), cycles=2 ** pace)
 
 
 def test_blink_duty(seg):
     print("Calamari Seg7 test: blink duty")
     from math import pow as fpow
     for pace in range(0, 7, 1):
-        seg.blink('@', period = 0.02, duty = 1.0/fpow(2, pace), elapse = 1.0)
+        seg.blink('@', period=0.02, duty=1.0/fpow(2, pace), elapse=1.0)
 
 
 def test_blinkc_duty(seg):
     print("Calamari Seg7 test: blinkc duty")
     from math import pow as fpow
     for pace in range(7, 0, -1):
-        seg.blinkc('@', period = 0.01, duty = 1.0/fpow(2, pace), cycles = int(1 / 0.01))
+        seg.blinkc('@', period=0.01, duty=1.0/fpow(2, pace), cycles=100)
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
 
     seg = Seg7()
 
@@ -273,4 +264,3 @@ if __name__== "__main__":
     test_blinkc_period(seg)
     test_blink_duty(seg)
     test_blinkc_duty(seg)
-    
